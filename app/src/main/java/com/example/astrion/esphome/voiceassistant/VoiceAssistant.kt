27e88a4f53c2382fb870ea00e0f64f0586d6bda9
老王杂谈说 <djhui@live.cn>
2026-09-17 22:47:48 +0800

    VoiceAssistant: missing Flow import

--- app/src/main/java/com/example/astrion/esphome/voiceassistant/VoiceAssistant.kt
+++ app/src/main/java/com/example/astrion/esphome/voiceassistant/VoiceAssistant.kt
@@ -20,6 +20,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
