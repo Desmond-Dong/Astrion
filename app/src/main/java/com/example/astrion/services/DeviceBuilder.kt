42bd3bd6824bc582160e54b4ca29705795193296
老王杂谈说 <djhui@live.cn>
2026-09-18 14:26:08 +0800

    Add missing filterNotNull import

--- app/src/main/java/com/example/astrion/services/DeviceBuilder.kt
+++ app/src/main/java/com/example/astrion/services/DeviceBuilder.kt
@@ -63,6 +63,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.flow.launchIn
