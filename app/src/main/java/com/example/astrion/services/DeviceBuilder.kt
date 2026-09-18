904c03c5607f4c6c3841b7693d63fb7059bdc586
老王杂谈说 <djhui@live.cn>
2026-09-18 14:19:49 +0800

    Add missing filter import

--- app/src/main/java/com/example/astrion/services/DeviceBuilder.kt
+++ app/src/main/java/com/example/astrion/services/DeviceBuilder.kt
@@ -62,6 +62,7 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.flow.launchIn
